71a40e4c19a243d525ba62b1e3f4f77620474cb9
juaniponce01 <62446939+juaniponce01@users.noreply.github.com>
2021-04-21 21:17:36 -0300

    Actualizacion con header

--- ejercitacion_definicion_clases/src/Recordatorios.cpp
+++ ejercitacion_definicion_clases/src/Recordatorios.cpp
@@ -1,38 +1,10 @@
 #include <iostream>
 #include <list>
-
-using namespace std;
-
-using uint = unsigned int;
-
-// Pre: 0 <= mes < 12
-uint dias_en_mes(uint mes) {
-    uint dias[] = {
-        // ene, feb, mar, abr, may, jun
-        31, 28, 31, 30, 31, 30,
-        // jul, ago, sep, oct, nov, dic
-        31, 31, 30, 31, 30, 31
-    };
-    return dias[mes - 1];
-}
+#include "Recordatorios.h"
 
 // Ejercicio 7, 8, 9 y 10
 
 // Clase Fecha
-class Fecha {
-  public:
-    // Completar declaraciones funciones
-    bool operator==(Fecha o);
-    Fecha(int mes, int dia);
-    uint mes();
-    uint dia();
-    void incrementar_dia();
-
-  private:
-    int mes_;
-    int dia_;
-};
-
 Fecha::Fecha(int mes, int dia) : mes_(mes), dia_(dia) {}
 
 uint Fecha::mes() {
@@ -75,19 +47,6 @@ void Fecha::incrementar_dia() {
 
 // Ejercicio 11, 12
 
-class Horario {
-  public:
-    Horario(uint hora, uint min);
-    uint hora();
-    uint min();
-    bool operator==(Horario o);
-    bool operator<(Horario o);
-
-  private:
-    int hora_;
-    int min_;
-};
-
 Horario::Horario(uint hora, uint min) : hora_(hora), min_(min) {}
 uint Horario::hora() {
     return hora_;
@@ -116,20 +75,6 @@ bool Horario::operator<(Horario o) {
 }
 
 // Ejercicio 13
-
-class Recordatorio {
-  public:
-    Recordatorio(Fecha f, Horario h, string s);
-    Fecha fecha();
-    Horario horario();
-    string mensaje();
-    bool operator==(Recordatorio o);
-
-  private:
-    Fecha fecha_;
-    Horario horario_;
-    string mensaje_;
-};
 
 Recordatorio::Recordatorio(Fecha f, Horario h, string s) : fecha_(f), horario_(h), mensaje_(s) {}
 
@@ -168,20 +113,6 @@ ostream& operator<<(ostream& os, Recordatorio r) {
 }
 
 // Ejercicio 14
-
-class Agenda {
-  public:
-    Agenda(Fecha fecha_inicial);
-    void agregar_recordatorio(Recordatorio rec);
-    void incrementar_dia();
-    list<Recordatorio> recordatorios_de_hoy();
-    Fecha hoy();
-
-  private:
-    Fecha hoy_;
-    list<Recordatorio> recordatorios_de_hoy_;
-
-};
 
 Agenda::Agenda(Fecha fecha_inicial) : hoy_(fecha_inicial) {}
 
--- ejercitacion_definicion_clases/src/Recordatorios.h
+++ ejercitacion_definicion_clases/src/Recordatorios.h
@@ -5,4 +5,83 @@
 #ifndef SOLUCION_RECORDATORIOS_H
 #define SOLUCION_RECORDATORIOS_H
 
+using namespace std;
+
+using uint = unsigned int;
+
+// Pre: 0 <= mes < 12
+uint dias_en_mes(uint mes) {
+    uint dias[] = {
+            // ene, feb, mar, abr, may, jun
+            31, 28, 31, 30, 31, 30,
+            // jul, ago, sep, oct, nov, dic
+            31, 31, 30, 31, 30, 31
+    };
+    return dias[mes - 1];
+}
+
+// Ejercicio 7, 8, 9 y 10
+
+// Clase Fecha
+class Fecha {
+public:
+    // Completar declaraciones funciones
+    bool operator==(Fecha o);
+    Fecha(int mes, int dia);
+    uint mes();
+    uint dia();
+    void incrementar_dia();
+
+private:
+    int mes_;
+    int dia_;
+};
+
+// Ejercicio 11, 12
+
+class Horario {
+public:
+    Horario(uint hora, uint min);
+    uint hora();
+    uint min();
+    bool operator==(Horario o);
+    bool operator<(Horario o);
+
+private:
+    int hora_;
+    int min_;
+};
+
+// Ejercicio 13
+
+class Recordatorio {
+public:
+    Recordatorio(Fecha f, Horario h, string s);
+    Fecha fecha();
+    Horario horario();
+    string mensaje();
+    bool operator==(Recordatorio o);
+
+private:
+    Fecha fecha_;
+    Horario horario_;
+    string mensaje_;
+};
+
+// Ejercicio 14
+
+class Agenda {
+public:
+    Agenda(Fecha fecha_inicial);
+    void agregar_recordatorio(Recordatorio rec);
+    void incrementar_dia();
+    list<Recordatorio> recordatorios_de_hoy();
+    Fecha hoy();
+
+private:
+    Fecha hoy_;
+    list<Recordatorio> recordatorios_de_hoy_;
+
+};
+
 #endif //SOLUCION_RECORDATORIOS_H

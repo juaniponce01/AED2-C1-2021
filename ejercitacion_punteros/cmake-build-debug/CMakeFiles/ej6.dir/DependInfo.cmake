838ceb08bfa4d355c61cfe4e1bfc9d7178546e64
juaniponce01 <62446939+juaniponce01@users.noreply.github.com>
2021-05-07 11:34:18 -0300

    primer commit

---
+++ ejercitacion_punteros/cmake-build-debug/CMakeFiles/ej6.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/src/ConexionJugador.cpp" "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/cmake-build-debug/CMakeFiles/ej6.dir/src/ConexionJugador.cpp.o"
+  "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/src/Internet.cpp" "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/cmake-build-debug/CMakeFiles/ej6.dir/src/Internet.cpp.o"
+  "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/src/Proxy2.cpp" "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/cmake-build-debug/CMakeFiles/ej6.dir/src/Proxy2.cpp.o"
+  "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/src/SistemaDeMensajes.cpp" "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/cmake-build-debug/CMakeFiles/ej6.dir/src/SistemaDeMensajes.cpp.o"
+  "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/tests/sdm_tests.cpp" "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/cmake-build-debug/CMakeFiles/ej6.dir/tests/sdm_tests.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "EJ=6"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/cmake-build-debug/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/cmake-build-debug/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ejercitacion_punteros/cmake-build-debug/CMakeFiles/ej7.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/src/ConexionJugador.cpp" "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/cmake-build-debug/CMakeFiles/ej7.dir/src/ConexionJugador.cpp.o"
+  "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/src/Internet.cpp" "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/cmake-build-debug/CMakeFiles/ej7.dir/src/Internet.cpp.o"
+  "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/src/SistemaDeMensajes.cpp" "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/cmake-build-debug/CMakeFiles/ej7.dir/src/SistemaDeMensajes.cpp.o"
+  "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/tests/sdm_tests_bonus.cpp" "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/cmake-build-debug/CMakeFiles/ej7.dir/tests/sdm_tests_bonus.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "EJ=7"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/cmake-build-debug/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/Users/juaniponce/Documents/GitHub/AED2-C1-2021/ejercitacion_punteros/cmake-build-debug/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
